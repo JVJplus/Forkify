9e682c83caf0f1c67d8bc66625aaeecc8eccc441
Jay Prakash Mahto <jvjplus@gmail.com>
2020-11-01 09:43:07 +0530

    webpack setuped

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./src/js/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "js/bundle.js",
+    },
+    devServer: {
+        contentBase: "./dist",
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            filename: "index.html",
+            template: "./src/index.html",
+        }),
+    ],
+};
