44858bbc482dda28772229f9ee96c396da4d6d65
Jay Prakash Mahto <jvjplus@gmail.com>
2020-11-12 08:14:47 +0530

    bundled the js

--- readme.md
+++ readme.md
@@ -1,34 +1,42 @@
 # Forkify
 
 ## Live Demo
+
 https://jvjplus.github.io/Forkify/
 
 ## Technologies
-* HTML5
-* CSS3
-* Vanilla JavaScript
-* Webpack
-* Babel
-* Axios
-* Fractional
-* Uniqid
+
+-   HTML5
+-   CSS3
+-   Vanilla JavaScript
+-   Webpack
+-   Babel
+-   Axios
+-   Fractional
+-   Uniqid
 
 ## General Info
+
 This app let you search for any recipe and gives you best results from web, you can also predict the quantity of ingredients required based on number of servings. You can also bookmark the favourite recipes. The user will then be able to add the necessary ingredients to shopping cart.
 
 App was created as part of [Jonas Schmedtmann's JavaScript course on Udemy](https://www.udemy.com/the-complete-javascript-course/learn).
 
-
 ## Special Features
-* Loading Spinners
-* Pagination
-* Bookmarks
-* Adding Items to cart based on no of servings
+
+-   Loading Spinners
+-   Pagination
+-   Bookmarks
+-   Adding Items to cart based on no of servings
+-   Empty state on respective views.
+-   Mobile Friendly
 
 ## Features That Can Be Added:
-* Make Mobile Friendly
-* Add empty state on respective views.
-* add hints like : try searching 'pizza, burger, chicken' with auto changing animation!
-* make option to add recipe using own api!
-* add delete all list items btn
-* add selective ingredients to list from recipe
+
+-   Hints like : try searching 'pizza, burger, chicken' with auto changing animation!
+-   make option to add recipe using own api!
+-   add delete all list items btn
+-   add selective ingredients to list from recipe
+
+## Redo
+
+-   responsivness & auto scroll.
--- src/js/views/base.js
+++ src/js/views/base.js
@@ -1,28 +1,30 @@
-export const elements={
-    searchInput:document.querySelector('.search__field'),
-    searchBtn:document.querySelector('.search__btn'),
-    search:document.querySelector('.search'),
-    results:document.querySelector('.results'),
-    resultsList:document.querySelector('.results__list'),
-    resultsPages:document.querySelector('.results__pages'),
-    recipeView:document.querySelector('.recipe'),
-    shopping:document.querySelector('.shopping__list'),
+export const elements = {
+    searchInput: document.querySelector('.search__field'),
+    searchBtn: document.querySelector('.search__btn'),
+    search: document.querySelector('.search'),
+    results: document.querySelector('.results'),
+    resultsList: document.querySelector('.results__list'),
+    resultsPages: document.querySelector('.results__pages'),
+    recipeView: document.querySelector('.recipe'),
+    shopping: document.querySelector('.shopping__list'),
     likesMenu: document.querySelector('.likes__field'),
-    likesList:document.querySelector('.likes__list'),
-    bookmarkIcon:document.querySelector('.likes__icon use'),
-    shoppingList: document.querySelector('.shopping')
+    likesList: document.querySelector('.likes__list'),
+    bookmarkIcon: document.querySelector('.likes__icon use'),
+    shoppingList: document.querySelector('.shopping'),
 };
 
-
 export const utilities = {
-  scrollOnClick: (scrollFrom, scrollTo) => {
-      if(scrollFrom){
-          scrollFrom.addEventListener('click', e =>{
-           const clearTime = setTimeout(() => {
-              scrollTo.scrollIntoView({ behavior: 'smooth', block: 'start'});            
-              clearTimeout(clearTime)
-            }, 400);
-        });
-    }
-  }
-}
+    scrollOnClick: (scrollFrom, scrollTo) => {
+        if (scrollFrom) {
+            scrollFrom.addEventListener('click', e => {
+                const clearTime = setTimeout(() => {
+                    scrollTo.scrollIntoView({
+                        behavior: 'smooth',
+                        block: 'start',
+                    });
+                    clearTimeout(clearTime);
+                }, 400);
+            });
+        }
+    },
+};
